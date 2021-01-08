49b36e4993513d05642e61402a2c78b7ea97fbc8
Cao Vy <caovy2001@gmail.com>
2021-01-09 00:44:53 +0700

    complete biggest rects

--- Program.js
+++ Program.js
@@ -1,4 +1,5 @@
 var table = new Table(4, 4);
+var biggest_rects_table = document.getElementById('biggest_rects');
 
 function SelectCell(name) {
     var idBtn = "btn" + name;
@@ -24,12 +25,8 @@ function RemoveColorForBtn(idBtn) {
     //document.getElementById(idBtn).style.backgroundColor = "#ccc4c4";
 }
 
-function AddTableContainBiggestRect(rect) {
-    var biggest_rects = document.getElementById('biggest_rects');
-
-    // create li (table of biggest rect)
-    var table_of_biggest_rect = document.createElement('li');
-    table_of_biggest_rect.setAttribute("class", "table_of_biggest_rect");
+function CreateChildTable(listOfChosenCells) {
+    var res = document.createElement('div');
 
     // create 16 div (cell)
     for (var i = 1; i <= 16; i++) {
@@ -39,8 +36,8 @@ function AddTableContainBiggestRect(rect) {
         var style = "";
         // make color for cell if it exists in listOfChosenCells 
         var checkExist = false;
-        for (var k = 0; k < rect.cells.length; k++)
-            if (rect.cells[k].name == i) {
+        for (var k = 0; k < listOfChosenCells.cells.length; k++)
+            if (listOfChosenCells.cells[k].name == i) {
                 checkExist = true;
                 break;
             }
@@ -55,35 +52,44 @@ function AddTableContainBiggestRect(rect) {
         cell.setAttribute('style', style);
 
         // add it to li (table of biggest rect)
-        table_of_biggest_rect.appendChild(cell);
+        res.appendChild(cell);
 
         // add line and endline
         if (i == 4 || i == 8 || i == 12 || i == 16)
-            table_of_biggest_rect.appendChild(document.createElement('br'));
+            res.appendChild(document.createElement('br'));
 
         if (i == 16) {
-            var h4 = document.createElement('h4');
-            var h4Style = "width: 100%; text-align: center; border-bottom: 1px solid #000; line-height: 0.1em; margin: 10px 0 20px;"
-            h4.setAttribute('style', h4Style);
 
-            var span = document.createElement('p');
-            var spanStyle = "background:#fff; padding:0 10px;";
-            span.setAttribute('style', spanStyle);
-            var xyztName = rect.XYZTName(8);
-            //span.appendChild(document.createTextNode(ConvRawFormuToStdFormu(xyztName)));
-            span.setAttribute('style', "font-family: 'Nunito', sans-serif;")
-            span.innerHTML = ConvRawFormuToStdFormu(xyztName);
-
-            h4.appendChild(span);
-            table_of_biggest_rect.appendChild(document.createElement('br'));
-            table_of_biggest_rect.appendChild(h4);
-            table_of_biggest_rect.appendChild(document.createElement('br'));
         }
-
-
     }
-    biggest_rects.appendChild(table_of_biggest_rect);
 
+    return res;
+}
+
+function AddTableContainBiggestRect(rect) {
+    // create li (table of biggest rect)
+    var table_of_biggest_rect_li = document.createElement('li');
+    table_of_biggest_rect_li.setAttribute("class", "table_of_biggest_rect");
+
+    var table_of_biggest_rect = CreateChildTable(rect);
+    var h4 = document.createElement('h4');
+    var h4Style = "width: 100%; text-align: center; border-bottom: 1px solid #000; line-height: 0.1em; margin: 10px 0 20px;"
+    h4.setAttribute('style', h4Style);
+
+    var span = document.createElement('p');
+    var spanStyle = "background:#fff; padding:0 10px;";
+    span.setAttribute('style', spanStyle);
+    var xyztName = rect.XYZTName(8);
+    span.setAttribute('style', "font-family: 'Nunito', sans-serif;")
+    span.innerHTML = ConvRawFormuToStdFormu(xyztName);
+
+    h4.appendChild(span);
+    table_of_biggest_rect.appendChild(document.createElement('br'));
+    table_of_biggest_rect.appendChild(h4);
+    table_of_biggest_rect.appendChild(document.createElement('br'));
+
+    table_of_biggest_rect_li.appendChild(table_of_biggest_rect);
+    biggest_rects_table.appendChild(table_of_biggest_rect_li);
 }
 
 function ConvRawFormuToStdFormu(rawFormuStr) {
@@ -108,9 +114,21 @@ function Solve() {
     strRes = ConvRawFormuToStdFormu(strRes);
 
     document.getElementById("result").innerHTML = strRes;
+
     var biggest_rects = table.BiggestRects();
-    for (var i = 0; i < biggest_rects.listOfRects.length; i++) {
-        AddTableContainBiggestRect(biggest_rects.listOfRects[i]);
-    }
+    if (biggest_rects.listOfRects.length != 0) {
+        document.getElementById("biggest_rects_section").style.display = "block";
+
+        // remove all element child of Biggest Rectangles section  
+        while (biggest_rects_table.lastElementChild)
+            biggest_rects_table.removeChild(biggest_rects_table.lastElementChild);
+
+        // add biggest rests to Biggest Rectangles section
+
+        for (var i = 0; i < biggest_rects.listOfRects.length; i++) {
+            AddTableContainBiggestRect(biggest_rects.listOfRects[i]);
+        }
+    } else
+        document.getElementById("biggest_rects_section").style.display = "none";
 
 }
